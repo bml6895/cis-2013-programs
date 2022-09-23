27bcb2ed2678fbe4099444bd24ca84be5ee05603
bml6895 <112423567+bml6895@users.noreply.github.com>
2022-09-23 11:17:40 -0500

    Add files via upload

---
+++ HO3C2.js
@@ -0,0 +1,8 @@
+var floatAge, floatDays, intWeeks, floatMonths, intFortnights;
+floatAge = prompt("Enter current age in years");
+floatDays = parseFloat(floatAge * 365.25);
+floatMonths = parseFloat(floatAge * 12);
+intWeeks = parseInt(floatAge/7);
+intFortnights = parseInt(floatDays/14);
+alert("Your age in Months, Fortnights, Weeks, Days = " + floatMonths, intFortnights, intWeeks, floatDays);
+
